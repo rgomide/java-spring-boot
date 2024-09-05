d612bc3746a0c5f2405aa256191c98213e3bd195
rgomide <renato.s.gomide@gmail.com>
2024-09-05 13:29:52 -0300

    .

--- 02-usando-os-verbos-do-http/src/main/java/br/com/gomide/controller/PersonController.java
+++ 02-usando-os-verbos-do-http/src/main/java/br/com/gomide/controller/PersonController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gomide.model.Person;
@@ -21,27 +24,27 @@ public class PersonController {
   @Autowired
   private PersonServices service;
 
-  @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Person> findAll() {
     return service.findAll();
   }
 
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Person findById(@PathParam(value = "id") String id) {
     return service.findById(id);
   }
 
-  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public Person create(@RequestBody Person person) {
     return service.create(person);
   }
 
-  @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public Person update(@RequestBody Person person) {
     return service.update(person);
   }
 
-  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public void delete(@PathVariable(value = "id") String id) {
     service.delete(id);
   }
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Sumário
 - [Primeiros passos com Spring Boot](./00-primeiros-passos-com-spring-boot/)
 - [Path params e tratamento de exceções](./01-path-parameters-e-exceptions/)
+- [Como usar os verbos HTTP no Spring](./02-usando-os-verbos-do-http/)
 
 # Referências
 - [Spring Boot](https://spring.io/projects/spring-boot)
